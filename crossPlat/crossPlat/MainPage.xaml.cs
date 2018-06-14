ad86143939e1cce92d6aab28c319056ce9265875
cesar augusto <augusto9623@hotmail.com>
2018-06-13 20:05:54 -0500

    Initial commit

---
+++ crossPlat/crossPlat/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace crossPlat
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            
+		}
+        private async void SayHelloButtonOnClicked(Object sender, EventArgs e)
+        {
+            string name = NameEntry.Text;
+            await DisplayAlert("Saludos", $"Hello {name}!"+$" nombre {SayHelloButton.Text}", "Cancel");
+        }
+	}
+}
